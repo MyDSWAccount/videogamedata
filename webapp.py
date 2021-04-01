9d8f441508ba3c90d50ca71645bb6cdeb3b6c2fb
MyDSWAccount <77691217+MyDSWAccount@users.noreply.github.com>
2021-04-01 10:05:32 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -7,8 +7,20 @@ def render_main():
     return render_template('home.html')
 
 @app.route("/pop")
-def render_main():
-    return render_template('popGame.html')
+def render_pop_games():
+    return render_template('popGame.html', options=get_years())
+
+def get_years():
+    listOfYears = []
+    with open('video_games.json') as vG_data:
+        videos = json.load(vG_data)
+    for video in videos:
+        if video["Release"]["Year"] not in listOfYears:
+            listOfYears.append(video["Release"]["Year"])
+    options = ""
+    for year in listOfYears:
+        options = options + Markup("<option value=\"" + year + "\">" + year + "</option>")
+    return options
 
 if __name__=="__main__":
     app.run(debug=False, port=54321)
