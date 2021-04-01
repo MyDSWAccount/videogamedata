0e484c2896047083981ab55b326e23144b7e0fd2
MyDSWAccount <77691217+MyDSWAccount@users.noreply.github.com>
2021-04-01 09:26:15 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -1,1 +1,10 @@
+from flask import Flask, url_for, render_template, request
 
+app = Flask(__name__)
+
+@app.route("/")
+def render_main():
+    return render_template('home.html')
+
+if __name__=="__main__":
+    app.run(debug=False, port=54321)
